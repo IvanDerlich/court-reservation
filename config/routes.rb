191274919d89ade543bbb8be37d63af897c96f10
Ivan Derlich <e@ivanderlich.com>
2021-03-29 23:20:13 -0300

    Experiments on contacts successful

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  # devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :todos do
     resources :items
   end
   namespace :v1 do
     resources :contacts
+    resources :sessions, only: [:create, :destroy]
   end
 end
