c62088a4749c161bc87d259689c89c33e92a5b28
Ivan Derlich <e@ivanderlich.com>
2021-05-11 10:43:42 -0300

    Get courts service ready

--- src/redux/services/courts/get.test.js
+++ src/redux/services/courts/get.test.js
@@ -1,5 +1,36 @@
-describe('Courts', () => {
-  it('Get all courts', () => {
+/* eslint-disable no-console */
+// Test all exports from the get file
+import signInService from '../auth/signIn';
+import {
+  getAllCourts,
+  // getOneCourt,
+  // getAllUserSCourts,
+} from './get';
 
+describe('Test Court Get methods', () => {
+  let headers;
+  beforeAll(done => {
+    const email = 'ivan@ivan.com';
+    const password = '12345678';
+    try {
+      signInService(email, password)
+        .then(response => {
+          headers = response.headers;
+          done();
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  });
+  describe('getAllCourts', () => {
+    it('Returns 10 courts', async () => {
+      try {
+        const response = await getAllCourts(headers);
+        expect(response.status).toBe(200);
+        expect(response.data.length).toBe(10);
+      } catch (e) {
+        console.log(e);
+      }
+    });
   });
 });
